4d883e852e5d306aa6240986378f8eb35c34daa4
Daniel Betancur-Echeverri <decheverri123@gmail.com>
2024-03-19 13:16:36 -0400

    Add xml2js library and update translation service

--- src/app/translation.service.ts
+++ src/app/translation.service.ts
@@ -1,35 +1,23 @@
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TranslationService {
-  constructor(private translate: TranslateService, private http: HttpClient) {}
+  constructor(private translate: TranslateService) {}
 
-  init() {
-    this.translate.setDefaultLang('en');
-    this.translate.use('en');
-
-    // Load translations dynamically using HttpClient
-    this.loadTranslations('en').subscribe((translations: any) => {
-      this.translate.setTranslation('en', translations);
-    });
-
-    this.loadTranslations('fr').subscribe((translations: any) => {
-      this.translate.setTranslation('fr', translations);
-    });
-  }
-
-  changeLang(lang: string) {
+  public setLanguage(lang: string): void {
     this.translate.use(lang);
   }
 
-  private loadTranslations(lang: string) {
-    return this.http
-      .get(`src/locale/messages.${lang}.json`)
-      .pipe(map((res: any) => res.default));
+  public getTranslation(key: string): string {
+    let translation: string = '';
+    this.translate.get(key).subscribe((res: string) => {
+      translation = res;
+    });
+    return translation;
   }
+
+  // Add more methods as needed to facilitate translation in your app
 }
